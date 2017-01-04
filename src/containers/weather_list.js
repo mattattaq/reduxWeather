cae5d38fa21a6b2a5079b9a60a4dd43e5ebb5252
mattattaq <mattatttaq@gmail.com>
2017-01-04 10:44:38 -0500

    working on api calls for the chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    // console.log(temps);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    console.log(temps);
+
     return (
         <tr key={name}>
           <td>{name}</td>
+          <td><Chart data={temps} color="orange" /></td>
+          <td><Chart data={pressures} color="salmon" /></td>
+          <td><Chart data={humidity} color="green" /></td>
         </tr>
     );
   }
-
   render() {
     return (
       <table className="table table-hover">
